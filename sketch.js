9f1f897ac53ad52707a20a3879d5cf72ec903be0
freeAhao <freeahao@qq.com>
2020-11-20 11:05:08 +0800

    t

--- sketch.js
+++ sketch.js
@@ -1,76 +1,40 @@
 let drawingLayer;
 let d2;
 let drawd1 = false;
-<<<<<<< HEAD
 var begin=1;
 var ignoreX=false;
 var ignoreY=false;
 var fa=10;
 var dot=false;
+var datas=[];
 
 function changeDot(){
-  dot = document.getElementById('ignoreDot').checked;
-  caldata();
+    dot = document.getElementById('ignoreDot').checked;
+    caldata();
 }
 
 function changeFa(){
-  fa = Number(document.getElementById('fa').value);
-  document.getElementById('fashow').innerHTML=fa;
-  caldata();
+    fa = Number(document.getElementById('fa').value);
+    document.getElementById('fashow').innerHTML=fa;
+    caldata();
 }
 
 function changeIgnore(xy){
-  if(xy=='x'){
-    ignoreX=document.getElementById('ignoreX').checked;
-  }else if(xy=='y'){
-    ignoreY=document.getElementById('ignoreY').checked;
-  }
-  caldata();
+    if(xy=='x'){
+        ignoreX=document.getElementById('ignoreX').checked;
+    }else if(xy=='y'){
+        ignoreY=document.getElementById('ignoreY').checked;
+    }
+    caldata();
 }
 function changeBegin(){
-  begin = Number(document.getElementById("beginIndex").value);
-  if (begin<=0){
-    begin = 1;
-    document.getElementById("beginIndex").value=1;
-  }
-  caldata();
-}
-
-function updateimage(){
-  var img = document.getElementById("img");
-  var file = img.files[0];
-  var url = null
-  if (window.createObjectURL != undefined) { // basic    
-    url = window.createObjectURL(file)
-  } else if (window.URL != undefined) { // mozilla(firefox)    
-    url = window.URL.createObjectURL(file)
-  } else if (window.webkitURL != undefined) { // webkit or chrome    
-    url = window.webkitURL.createObjectURL(file)
-  }
-  loadImage(url,drawImg)
-}
-
-function keyReleased(){
-  if( key == 'q'){
-    d2.clear();
-
-    datas=[];
+    begin = Number(document.getElementById("beginIndex").value);
+    if (begin<=0){
+        begin = 1;
+        document.getElementById("beginIndex").value=1;
+    }
     caldata();
-  }
-  return false;
 }
-
-function setup(){
-  var width=1920;
-  var height=1080;
-  var mcanvas = createCanvas(width,height);
-  drawingLayer = createGraphics(width, height);
-  d2 = createGraphics(width, height);
-  mcanvas.parent("container");
-  //loadImage("http://img0.pclady.com.cn/pclady/1505/15/1303475_292.jpg", drawCat);
-  //loadImage("/r.png", drawCat);
-  mcanvas.mouseReleased(mmouseReleased);
-=======
 
 function updateimage(){
     var img = document.getElementById("img");
@@ -85,8 +49,9 @@ function updateimage(){
     }
     loadImage(url,drawImg)
 }
+
 function keyReleased(){
-    if( key == 'c'){
+    if( key == 'q'){
         d2.clear();
 
         datas=[];
@@ -105,91 +70,58 @@ function setup(){
     //loadImage("http://img0.pclady.com.cn/pclady/1505/15/1303475_292.jpg", drawCat);
     //loadImage("/r.png", drawCat);
     mcanvas.mouseReleased(mmouseReleased);
->>>>>>> 4b96c450f36435ffb8fcbf40321124362f1a5865
 }
-
-var datas=[];
 
 function caldata(){
-<<<<<<< HEAD
-  var textArea = document.getElementById("result");
-  textArea.value="";
-  for(var i=1;i<datas.length;i++){
-    var number = i+begin-1;
-    var current = datas[i];
-    var last = datas[i-1];
-
-    if(ignoreX){
-      offsetX=0;
-    }else{
-      offsetX = ((current[0]-last[0])/-1);
-      offsetX = offsetX/fa;
-      if(dot){
-        offsetX=Math.round(offsetX);
-      }
-    }
-
-    if(ignoreY){
-      offsetY=0;
-    }else{
-      offsetY = ((current[1]-last[1])/-1);
-      offsetY = offsetY/fa;
-      if(dot){
-        offsetY=Math.round(offsetY);
-      }
-    }
-
-
-    console.log(number,offsetY, offsetX);
-    textArea.value+="{"+number+",";
-    textArea.value+=offsetY+",";
-    textArea.value+=offsetX+",";
-    textArea.value+="}"+",";
-    textArea.value+="\n";
-  }
-}
-
-function datapush(x, y){
-  var flag = false;
-  datas.forEach(function (i){
-    if(i[0]==x&&i[1]==y){
-      flag=true;
-    }
-  })
-
-  if (!flag){
-    datas.push([x,y]);
-    //console.log(datas);
-    caldata();
-  }
-}
-
-function mmouseReleased(event){
-  drawingLayer.noStroke();
-  drawingLayer.ellipse(mouseX, mouseY, 20);
-  d2.noStroke();
-  d2.stroke(255, 0, 0);
-  d2.ellipse(mouseX, mouseY, 10, 10);
-  datapush(mouseX,mouseY);
-=======
     var textArea = document.getElementById("result");
     textArea.value="";
     for(var i=1;i<datas.length;i++){
+        var number = i+begin-1;
         var current = datas[i];
         var last = datas[i-1];
 
-        offsetX = ((current[0]-last[0])/-1);
-        offsetY = ((current[1]-last[1])/-1);
+        if(ignoreX){
+            offsetX=0;
+        }else{
+            offsetX = ((current[0]-last[0])/-1);
+            offsetX = offsetX/fa;
+            if(dot){
+                offsetX=Math.round(offsetX);
+            }
+        }
 
-        offsetX = Math.round(offsetX/10);
-        offsetY = Math.round(offsetY/10);
+        if(ignoreY){
+            offsetY=0;
+        }else{
+            offsetY = ((current[1]-last[1])/-1);
+            offsetY = offsetY/fa;
+            if(dot){
+                offsetY=Math.round(offsetY);
+            }
+        }
 
-        console.log(i,offsetY, offsetX);
-        textArea.value+="{"+i+",";
+
+        console.log(number,offsetY, offsetX);
+        textArea.value+="{"+number+",";
         textArea.value+=offsetY+",";
         textArea.value+=offsetX+",";
         textArea.value+="}"+",";
         textArea.value+="\n";
+    }
+}
+
+function datapush(x, y){
+    var flag = false;
+    datas.forEach(function (i){
+        if(i[0]==x&&i[1]==y){
+            flag=true;
+        }
+    })
+
+    if (!flag){
+        datas.push([x,y]);
+        //console.log(datas);
+        caldata();
     }
 }
 
@@ -215,27 +147,23 @@ function mmouseReleased(event){
     d2.stroke(255, 0, 0);
     d2.ellipse(mouseX, mouseY, 10, 10);
     datapush(mouseX,mouseY);
->>>>>>> 4b96c450f36435ffb8fcbf40321124362f1a5865
 }
 
 function draw(){
-  background(220);
+    background(220);
 
-  if (drawd1) {
-    image(drawingLayer, 0, 0);
-  } else if (!drawd1) {
-    image(d2, 0, 0);
-  }
+    if (drawd1) {
+        image(drawingLayer, 0, 0);
+    } else if (!drawd1) {
+        image(d2, 0, 0);
+    }
 
-  noFill();
-  ellipse(mouseX, mouseY, 20);
+    noFill();
+    ellipse(mouseX, mouseY, 20);
 }
 
 function drawImg(img){
-<<<<<<< HEAD
-  d2.image(img, 0, 0);
-=======
     d2.image(img, 0, 0);
->>>>>>> 4b96c450f36435ffb8fcbf40321124362f1a5865
+    d2.image(img, 0, 0);
 }
 
